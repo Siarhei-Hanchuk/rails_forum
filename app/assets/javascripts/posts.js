6eb7cd09ffeeda4bda09953df23fced7081e60be
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2012-10-14 16:48:01 +0300

    try fix js

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -2,7 +2,7 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$(document).ready(function() {
-	$("#table tr:nth-child(odd)").css("background-color","#dad0ff");
-	$("#table tr:nth-child(even)").css("background-color","#fefefe");
-});
+//#$(document).ready(function() {
+//#	$("#table tr:nth-child(odd)").css("background-color","#dad0ff");
+//#	$("#table tr:nth-child(even)").css("background-color","#fefefe");
+//#});
