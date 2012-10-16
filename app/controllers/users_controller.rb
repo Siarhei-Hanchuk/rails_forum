c482d12154fd38f6db9ec7f1ef3ca19867d2eea4
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2012-10-16 20:14:27 +0300

    fix welcom_email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     @user.status=0;
-    UserMailer.welcom_email(@user).deliver
+    UserMailer.welcome_email(@user).deliver
     respond_to do |format|
       if @user.save
         session[:user_id]=@user.id
