90c60c6c371516adeba6d98f58eff264cbb70193
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2012-10-18 23:35:08 +0300

    auth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,9 @@
 class Users::OmniauthCallbacksController < ApplicationController
 	def facebook
-		<hh user=user> = User.find_for_facebook_oauth request.env["omniauth.auth"]
-		if <hh user=user>.persisted?
+		@user = User.find_for_facebook_oauth request.env["omniauth.auth"]
+		if @user.persisted?
 			flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-			sign_in_and_redirect <hh user=user>, :event => :authentication
+			sign_in_and_redirect @user :event => :authentication
 		else
 			flash[:notice] = "authentication error"
 			redirect_to root_path
