43f3a1f550a0f35a8db2579dd37dcc1c77a2e278
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2012-11-03 14:21:46 +0300

    add devise support to rsper

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,11 @@
+module ControllerMacros
+  def login role = nil
+    before(:each) do
+      user=User.create
+      user.roles_mask=8
+      user.roles_mask=11 if role==:admin
+      user.save
+      session[:user_id]=user.id
+    end
+  end
+end
